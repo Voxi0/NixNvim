bef5a928e54db77ceb9111006109bbf2cc5ce2c7
Voxi0 <alif200099@gmail.com>
2025-04-19 00:02:30 +0100

    Cleaning `opts.lua`
    Reformatted it to very visibly separate globals and options. I also
    replaced all instances of `vim.o` with `vim.opt` as it's supposed to be
    a "modern" replacement and is recommended to use. I'll use `vim.opt`
    whenever possible from now on. I'm not entirely sure how it's better but
    it is, if you wanna know go Google or smth.

--- lua/opts.lua
+++ lua/opts.lua
@@ -1,51 +1,52 @@
+-- #############
+-- ## GLOBALS ##
+-- #############
+-- Disable the default Neovim file explorer (REQUIRED) - We'll use something else
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- #############
+-- ## OPTIONS ##
+-- #############
 -- Decrease update time for performance
-vim.o.updatetime = 250
-vim.o.timeoutlen = 300
+vim.opt.updatetime = 250
+vim.opt.timeoutlen = 300
 
 -- Nice to have
 vim.opt.fillchars = { eob = ' ' } -- Set what character to display for empty lines
-vim.o.termguicolors = true -- Ensure that Neovim shows the "best" version of a colorscheme if the terminal supports it
-vim.o.spell = true -- Spell checking
-vim.o.wrap = false -- Don't wrap text around the screen horizontally
-vim.o.mouse = 'a' -- Mouse mode
-vim.o.clipboard = 'unnamedplus' -- Clipboard
-vim.o.completeopt = 'menu,preview,noselect' -- For a better completion experience
-vim.o.undofile = true -- Save undo history
-vim.o.breakindent = true -- Stops line wrapping from being confusing
+vim.opt.termguicolors = true -- Ensure that Neovim shows the "best" version of a colorscheme if the terminal supports it
+vim.opt.spell = true -- Spell checking
+vim.opt.wrap = false -- Don't wrap text around the screen horizontally
+vim.opt.mouse = 'a' -- Mouse mode
+vim.opt.clipboard = 'unnamedplus' -- Clipboard
+vim.opt.completeopt = 'menu,preview,noselect' -- For a better completion experience
+vim.opt.undofile = true -- Save undo history
+vim.opt.breakindent = true -- Stops line wrapping from being confusing
 vim.opt.scrolloff = 10 -- Minimal number of screen lines to keep above and below the cursor
 vim.opt.inccommand = 'split' -- Preview substitutions live as you type
 
 -- Highlight on search and enable case-insensitive searching UNLESS \C or capital in search
 vim.opt.hlsearch = true
-vim.o.ignorecase = true
-vim.o.smartcase = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
 
 -- Signcolumn and line numbers
-vim.wo.signcolumn = 'yes'
-vim.wo.number = true
-vim.wo.relativenumber = true
-
--- Sets how neovim will display certain whitespace characters in the editor.
-vim.opt.list = true
-vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
-
--- Disable the default Neovim file explorer (REQUIRED) - We use Neotree instead
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
+vim.opt.signcolumn = 'yes'
+vim.opt.number = true
+vim.opt.relativenumber = true
 
 -- Indentation
--- vim.o.smarttab = true
--- vim.o.smartindent = true
--- vim.o.autoindent = true
--- vim.o.softtabstop = 4
-vim.o.expandtab = false
-vim.o.tabstop = 2
-vim.o.shiftwidth = 2
-vim.opt.cpoptions:append 'I'
+-- vim.opt.smarttab = true
+-- vim.opt.smartindent = true
+-- vim.opt.autoindent = true
+-- vim.opt.softtabstop = 4
+vim.opt.expandtab = false
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
 
 -- Code folding
-vim.o.foldenable = true
-vim.o.foldcolumn = '0'
-vim.o.foldtext = ''
-vim.o.foldlevel = 99
-vim.o.foldlevelstart = 99
+vim.opt.foldenable = true
+vim.opt.foldcolumn = '0'
+vim.opt.foldtext = ''
+vim.opt.foldlevel = 99
+vim.opt.foldlevelstart = 99
