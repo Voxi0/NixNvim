3d440ad0d499e48c48dfca5d05e353ce97a3f3ad
Voxi0 <alif200099@gmail.com>
2025-06-15 09:08:44 +0100

    Code Folding
    Implemented code folding with the `nvim-ufo` plugin. Also updated the
    flake as always.

--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -16,4 +16,24 @@ return {
 			}
 		end,
 	},
+
+	-- Code folding
+	{
+		"nvim-ufo",
+		after = function()
+			-- Configure code folding
+			opts = vim.o
+			opts.foldcolumn = "1"
+			opts.foldlevel = 99
+			opts.foldlevelstart = 99
+			opts.foldenable = true
+
+			-- Must remap `zR` and `zM` if using Nvim-UFO provider
+			vim.keymap.set("n", "zR", require("ufo").openAllFolds)
+			vim.keymap.set("n", "zM", require("ufo").closeAllFolds)
+
+			-- Initialize Nvim UFO
+			require("ufo").setup()
+		end,
+	},
 }
