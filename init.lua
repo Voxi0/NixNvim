211f3105ca0f0326806e3e58594065f6486d0443
voxi0 <alif200099@gmail.com>
2025-07-17 18:10:34 +0100

    fix: fixed neocord

--- init.lua
+++ init.lua
@@ -2,7 +2,7 @@
 require "opts"
 require "keymaps"
 require "autocmds"
-require("lze").load "plugins"
+require("lze").load("plugins")
 
 -- Set the theme/colorscheme
 vim.cmd.colorscheme(nixCats "colorscheme")
