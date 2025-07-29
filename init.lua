fc8d0e8fee43d6bb8a01e8b6391a151d20b0c537
voxi0 <alif200099@gmail.com>
2025-07-29 18:19:39 +0100

    Moving Over to NVF
    It's just way easier to use and since I use Nix a lot might as well use
    Nix when possible no?

--- init.lua
+++
@@ -1,8 +0,0 @@
--- Import everything
-require "opts"
-require "keymaps"
-require "autocmds"
-require("lze").load "plugins"
-
--- Set the theme/colorscheme
-vim.cmd.colorscheme(nixCats "colorscheme")
