7454abf5e5ae12705af392f015aa5a1075b3e487
Voxi0 <alif200099@gmail.com>
2025-08-04 01:01:46 +0100

    feat: code folding
    Code folding with the Nvim-UFO plugin and just cleaning up. Updated the
    flake as well because why not?

--- src/core/ui.nix
+++ src/core/ui.nix
@@ -3,30 +3,56 @@ _: {
   ### APPEARANCE ###
   ##################
   vim = {
-    dashboard.alpha.enable = true;
-    statusline.lualine.enable = true;
-    notify.nvim-notify.enable = true;
+    # Colorscheme
     theme = {
       enable = true;
       name = "catppuccin";
       style = "mocha";
     };
 
-    # Tabline - Shows open file buffers
+    # Dashboard/Greeter
+    dashboard.alpha.enable = true;
+
+    # Statusline
+    statusline.lualine.enable = true;
+
+    # Fancy and configurable notification manager
+    notify.nvim-notify.enable = true;
+
+    # Shows open file buffers
     tabline.nvimBufferline.enable = true;
 
     # UI
     ui = {
+      # Replaces the UI for messages, cmdline and the popupmenu
       noice.enable = true;
+
+      # Code folding
+      nvim-ufo.enable = true;
+
+      # Provides code context in the winbar
       breadcrumbs.enable = true;
+
+      # Color highlighter
       colorizer.enable = true;
+
+      # Offers a simple and intuitive interface for code actions
       fastaction.enable = true;
     };
     visuals = {
+      # Icons
       nvim-web-devicons.enable = true;
+
+      # Indentation guides
       indent-blankline.enable = true;
+
+      # Extensible UI for Neovim notifications and LSP progress messages
       fidget-nvim.enable = true;
+
+      # Execute cellular automaton animations based on the content of neovim buffer
       cellular-automaton.enable = true;
+
+      # Smooth-scrolling for all commands
       cinnamon-nvim.enable = true;
     };
   };
